c86e1b8c3075dcee95d29cb77284992fd865c927
Joe Mattiello <mail@joemattiello.com>
2023-02-16 03:06:12 -0500

    Swift lint and format

--- Sources/Roxas_tvOS/Roxas_tvOS.swift
+++ Sources/Roxas_tvOS/Roxas_tvOS.swift
@@ -2,21 +2,21 @@ import Foundation
 
 @objcMembers
 @objc public final class Roxas_tvOS: NSObject {
-    @objc public class var resources: Bundle {
-#if FRAMEWORK
-        let bundle = Bundle(for: Roxas_tvOS.self)
-#elseif SWIFT_PACKAGE
-        let bundle = Bundle.module
-#elseif STATIC_LIBRARY
-        let bundle: Bundle
-        if let bundleURL = Bundle.main.url(forResource: "Roxas_tvOS", withExtension: "bundle") {
-            bundle = Bundle(url: bundleURL)!
-        } else {
-            bundle = .main
-        }
-#else
-        let bundle = Bundle.main
-#endif
+    public class var resources: Bundle {
+        #if FRAMEWORK
+            let bundle = Bundle(for: Roxas_tvOS.self)
+        #elseif SWIFT_PACKAGE
+            let bundle = Bundle.module
+        #elseif STATIC_LIBRARY
+            let bundle: Bundle
+            if let bundleURL = Bundle.main.url(forResource: "Roxas_tvOS", withExtension: "bundle") {
+                bundle = Bundle(url: bundleURL)!
+            } else {
+                bundle = .main
+            }
+        #else
+            let bundle = Bundle.main
+        #endif
 
         return bundle
     }
